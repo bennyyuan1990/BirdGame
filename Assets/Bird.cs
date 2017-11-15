03c39c07533037ff16b8244038ee82c964be6ec5
benny <bennyyuan1990@sina.com>
2017-11-15 08:29:03 +0800

    init

---
+++ Assets/Bird.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private PlayMakerFSM _playMakerFSM;
+
+    private Rigidbody2D _rigidbody2D;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _playMakerFSM = GetComponent<PlayMakerFSM>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void SetForce(Vector2 power)
+    {
+        Vector2 vector = _rigidbody2D.velocity;
+        vector.y = 0;
+        _rigidbody2D.velocity = vector;
+        _rigidbody2D.AddForce(power);
+
+    }
+
+ 
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayMakerFSM.BroadcastEvent("GameOver");
+       
+    }
+}
